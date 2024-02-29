ce49c0fcffe4eef5097ae4a51d99fb9e4cf6d000
Viraat <viraat.laldas@gmail.com>
2024-02-29 01:55:42 -0500

    started as a xcode app

---
+++ MetalShading/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalShading
+//
+//  Created by Viraat Das on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalShading/MetalShadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShadingApp.swift
+//  MetalShading
+//
+//  Created by Viraat Das on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalShadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
